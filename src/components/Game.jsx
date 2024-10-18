1700696c088759dcb2da1506d33e8f8ac4a2ea7d
Zeronicc <zeronic4465@gmail.com>
2024-10-17 19:06:02 -0500

    test

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -3,7 +3,7 @@ import '../styles/Game.css';
 
 function StartGame({range, score, setScore}) {
     const [data, setData] = useState(false);
-    const key = import.meta.env.VITE_API_KEY;
+    const key = process.env.VITE_API_KEY;
     useEffect(() => {
         fetch(`https://api.giphy.com/v1/gifs/trending?api_key=${key}&limit=${range}&offset=0&rating=g&bundle=messaging_non_clips`)
             .then(response => {
